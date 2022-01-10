5e60b41008e83b179c5c44d5a95428a0c070dec5
Behnam Parsaeifard <parsaeif@i63.ib0.cm.izar.local>
2022-01-10 01:33:13 +0100

    test

---
+++ DataLoader.py
@@ -0,0 +1,140 @@
+import torch
+import torchvision
+import torchvision.transforms.functional as TF
+import pandas as pd
+from ast import literal_eval
+import glob
+import os
+import numpy as np
+from PIL import Image, ImageDraw
+import time
+
+class myJAAD(torch.utils.data.Dataset):
+    def __init__(self, args):
+        print('Loading', args.dtype, 'data ...')
+        
+        if(args.from_file):
+            sequence_centric = pd.read_csv(args.file).head(5)
+            df = sequence_centric.copy()      
+            for v in list(df.columns.values):
+                print(v+' loaded')
+                try:
+                    df.loc[:,v] = df.loc[:, v].apply(lambda x: literal_eval(x))
+                except:
+                    continue
+            sequence_centric[df.columns] = df[df.columns]
+            
+        else:
+            print('Not available')
+            exit()
+ 
+        self.data = sequence_centric.copy().reset_index(drop=True)
+        self.args = args
+        self.dtype = args.dtype
+        print(args.dtype, "set loaded")
+        print('*'*30)
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index):
+
+        seq = self.data.iloc[index]
+        outputs = []
+        ########################################################## 
+        scene_paths = [os.path.join("/work/vita/JAAD_clips_bodyposes_by_openpifpaf/images/"+seq["scenefolderpath"][frame],"0"+ seq["filename"][frame]) 
+                           for frame in range(0,self.args.input,self.args.skip)]
+        scene_paths_t = [os.path.join("/work/vita/JAAD_clips_bodyposes_by_openpifpaf/images/"+seq["scenefolderpath_t"][frame], "0"+seq["filename_t"][frame]) 
+                           for frame in range(0,self.args.output,self.args.skip)]
+      
+        observed_keypoints = torch.tensor(np.array(seq.keypoints))
+        future_keypoints = torch.tensor(np.array(seq.future_keypoints))
+        obs_keypoints = torch.tensor([seq.keypoints[i] for i in range(0,self.args.input,self.args.skip)])
+        obs_speed_keypoints = (obs_keypoints[1:] - obs_keypoints[:-1])
+        outputs.append(obs_speed_keypoints.type(torch.float32))
+        if 'body_pos' in self.args.task:
+            true = torch.tensor([seq.future_keypoints[i] for i in range(0,self.args.output,self.args.skip)])
+            true_speed = torch.cat(((true[0]-obs_keypoints[-1]).unsqueeze(0), true[1:]-true[:-1]))
+            outputs.append(true_speed.type(torch.float32))
+            outputs.append(obs_keypoints.type(torch.float32))
+            outputs.append(true.type(torch.float32))
+
+        ########################################################## 
+
+        observed = torch.tensor(np.array(seq.bounding_box))
+        future = torch.tensor(np.array(seq.future_bounding_box))
+        
+        obs = torch.tensor([seq.bounding_box[i] for i in range(0,self.args.input,self.args.skip)])
+        obs_speed = (obs[1:] - obs[:-1])
+    
+        outputs.append(obs_speed.type(torch.float32))
+        
+        if 'bounding_box' in self.args.task:
+            true = torch.tensor([seq.future_bounding_box[i] for i in range(0,self.args.output,self.args.skip)])
+            true_speed = torch.cat(((true[0]-obs[-1]).unsqueeze(0), true[1:]-true[:-1])) #Question
+            outputs.append(true_speed.type(torch.float32))
+            outputs.append(obs.type(torch.float32))
+            outputs.append(true.type(torch.float32))
+        
+        if 'intention' in self.args.task:
+            obs_cross = torch.tensor([seq.crossing_obs[i] for i in range(0,self.args.input,self.args.skip)])
+            obs_non_cross = torch.ones(obs_cross.shape, dtype=torch.int64)-obs_cross
+            obs_cross = torch.cat((obs_non_cross.unsqueeze(1), obs_cross.unsqueeze(1)), dim=1)
+
+            true_cross = torch.tensor([seq.crossing_true[i] for i in range(0,self.args.output,self.args.skip)])
+            true_non_cross = torch.ones(true_cross.shape, dtype=torch.int64)-true_cross
+            true_cross = torch.cat((true_non_cross.unsqueeze(1), true_cross.unsqueeze(1)), dim=1)
+            cross_label = torch.tensor(seq.label)
+            outputs.append(obs_cross.type(torch.float32))
+            outputs.append(true_cross.type(torch.float32))
+            outputs.append(cross_label.type(torch.float32))
+
+        if self.args.use_scenes:     
+            scene_paths = [os.path.join(seq["scenefolderpath"][frame], '%.4d'%seq.ID, seq["filename"][frame]) 
+                           for frame in range(0,self.args.input,self.args.skip)]
+        
+            for i in range(len(scene_paths)):
+                scene_paths[i] = scene_paths[i].replace('haziq-data', 'smailait-data').replace('scene', 'resized_scenes')
+
+            scenes = torch.tensor([])
+            for i, path in enumerate(scene_paths):
+                scene = Image.open(path)
+                #bb = obs[i,:]
+                #img = ImageDraw.Draw(scene)   
+                #utils.drawrect(img, ((bb[0]-bb[2]/2, bb[1]-bb[3]/2), (bb[0]+bb[2]/2, bb[1]+bb[3]/2)), width=5)
+                scene = self.scene_transforms(scene)
+                scenes = torch.cat((scenes, scene.unsqueeze(0)))
+                
+            outputs.insert(0, scenes)
+        outputs.insert(0, scene_paths)
+        outputs.insert(1, scene_paths_t)
+        
+        return tuple(outputs)
+
+    def scene_transforms(self, scene):  
+        #scene = TF.resize(scene, size=(self.args.image_resize[0], self.args.image_resize[1]))
+        scene = TF.to_tensor(scene)
+        
+        return scene
+
+def my_collate(batch):
+
+    (obs_f, target_f, obs_s, target_s, obs_p, target_p, _, _, _, _, obs_c, target_c, label_c) = zip(*batch)
+    obs_p = torch.stack(obs_p).permute(1,0,2)
+    obs_s = torch.stack(obs_s).permute(1,0,2)
+    target_p = torch.stack(target_p).permute(1,0,2)
+    target_s = torch.stack(target_s).permute(1,0,2)
+    obs_c = torch.stack(obs_c).permute(1,0,2)
+    target_c = torch.stack(target_c).permute(1,0,2)
+    #label_c = torch.stack(label_c)
+    out = [obs_p, obs_s, obs_f, target_p, target_s, target_f, obs_c, target_c, label_c]
+    return tuple(out)
+
+    
+def data_loader(args):
+    dataset = myJAAD(args)
+    dataloader = torch.utils.data.DataLoader(
+        dataset, batch_size=args.batch_size, shuffle=args.loader_shuffle,
+        pin_memory=args.pin_memory, num_workers=args.loader_workers, collate_fn=my_collate, drop_last=False)
+
+    return dataloader
---
+++ train.py
@@ -0,0 +1,195 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import torch.optim as optim
+import time
+import DataLoader
+import datetime
+
+#device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+device = torch.device('cpu')
+filename_save = "checkpoint_lstm.pkl"
+l2_lambda = 0.
+
+def ade(pred, true):
+    seq_len, batch, l = true.shape
+    return torch.sum(torch.sqrt(torch.sum((pred-true)**2, dim=-1) / l//2 ))/seq_len
+
+def fde(pred, true):
+    seq_len, batch, l = true.shape
+    return torch.sum(torch.sqrt(torch.sum((pred[-1]-true[-1])**2, dim=-1) / l//2 ))
+
+class LSTM(nn.Module):
+    def __init__(self, pose_dim=34, h_dim=16, num_layers=1, dropout=0.2):
+        super(LSTM, self).__init__()
+
+        self.pose_dim = pose_dim
+        self.h_dim = h_dim
+        self.num_layers = num_layers
+        self.device = device 
+        self.encoder = nn.LSTM(pose_dim, h_dim, num_layers, dropout=dropout)
+        self.decoder = nn.LSTM(pose_dim, h_dim, num_layers, dropout=dropout)
+        self.fc = nn.Sequential(nn.Linear(h_dim, pose_dim))
+
+    def forward(self, obs_s=None, pred_len=15):
+
+        seq_len, batch, l = obs_s.shape
+        assert(l == self.pose_dim)
+        state_tuple = (torch.zeros(self.num_layers, batch, self.h_dim, device=self.device, dtype=torch.float64),
+                       torch.zeros(self.num_layers, batch, self.h_dim, device=self.device, dtype=torch.float64))
+
+        output, state_tuple = self.encoder(obs_s, state_tuple)
+
+        pred_s = torch.tensor([], device=self.device, dtype=torch.float64)
+        last_s = obs_s[-1].unsqueeze(0)
+        for i in range(pred_len):
+
+             output, state_tuple = self.decoder(last_s, state_tuple)
+             curr_s = self.fc(output[-1])
+             pred_s = torch.cat((pred_s, curr_s.unsqueeze(0)), dim=0)
+             last_s = curr_s.detach().unsqueeze(0)
+
+        return pred_s
+
+
+def training_loop(n_epochs, optimizer, scheduler, model, loss_fn, train_loader, val_loader):
+
+    for epoch in range(1, n_epochs + 1):
+
+        loss_train = 0.0
+        ade_train = 0.0
+        fde_train = 0.0
+        count_train = 0
+        start = time.time()
+        for idx, (obs_p, obs_s, obs_f, target_p, target_s, target_f, obs_c, target_c, label_c) in enumerate(train_loader):
+
+            obs_p = obs_p.to(device=device).double() 
+            obs_s = obs_s.to(device=device).double()
+            target_p = target_p.to(device=device).double() 
+            target_s = target_s.to(device=device).double()
+
+            pred_s = model(obs_s=obs_s)
+            loss = loss_fn(pred_s, target_s)
+
+            l2_norm = sum(p.pow(2.0).sum() for p in model.parameters()) 
+            loss = loss + l2_lambda * l2_norm
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            pred_p = obs_p[-1].unsqueeze(0) + torch.cumsum(pred_s.detach(), dim=0)
+            #pred_p[0] = obs_p[-1] + pred_s[0]
+            #pred_p[1] = pred_p[0] + pred_s[1] = obs_p[-1] + pred_s[0] + pred_s[1] = 
+            #pred_p[2] = pred_p[1] + pred_s[2] = obs_p[-1] + pred_s[0] + pred_s[1] + pred_s[2]
+            #      ...
+            #pred_p[i] = pred_p[i-1] + pred_s[i] = obs_p[-1] + torch.cumsum(pred_s, dim=0)
+            
+            batch = obs_p.size(1)
+            count_train += batch 
+            loss_train += loss.item()*batch
+            ade_train += float(ade(pred_p, target_p))
+            fde_train += float(fde(pred_p, target_p))
+
+        loss_train /= count_train
+        ade_train /= count_train
+        fde_train /= count_train    
+        scheduler.step(loss_train)
+
+        loss_val = 0.0
+        ade_val = 0.0
+        fde_val = 0.0
+        count_val = 0
+        with torch.no_grad():
+
+            for idx, (obs_p, obs_s, obs_f, target_p, target_s, target_f, obs_c, target_c, label_c) in enumerate(val_loader):
+                obs_p = obs_p.to(device=device).double() 
+                obs_s = obs_s.to(device=device).double()
+                target_p = target_p.to(device=device).double() 
+                target_s = target_s.to(device=device).double()
+  
+                pred_s = model(obs_s=obs_s)
+                loss = loss_fn(pred_s, target_s)
+                
+                count_val += obs_p.size(1)
+                loss_val += loss.item()*batch
+                ade_val += float(ade(pred_p, target_p))
+                fde_val += float(fde(pred_p, target_p))
+
+            loss_val /= count_val
+            ade_val /= count_val
+            fde_val /= count_val
+
+        if(epoch % 10 == 0):
+            #print(f'Saving ... at epoch {epoch}')
+            #torch.save(model.state_dict(), filename_save)
+            print("date: %s "%datetime.datetime.now(),
+                  "|e: %d "%epoch,
+                  "|loss_t: %0.4f "%loss_train,
+                  "|loss_v: %0.4f "%loss_val,
+                  "|fde_t: %0.4f "%fde_train,
+                  "|fde_v: %0.4f "%fde_val,
+                  "|ade_t: %0.4f "%ade_train, 
+                  "|ade_v: %0.4f "%ade_val, 
+                  "|g_norm: %0.8f"%np.sqrt(sum([p.grad.data.norm(2).item() for p in model.parameters()])),
+                  "|l_reg: %0.8f"%l2_norm,
+                  "|time(s): %0.2f "%(time.time()-start)) 
+
+
+class Args():
+   def __init__(self, batch_size=200, loader_shuffle=True, pin_memory=False, loader_workers=1, load_checkpoint=False,
+             jaad_dataset='',
+             #jaad_dataset='/home/parsaeif/bounding-box-prediction-bodyposes/JAAD/processed_annotations',
+             dtype='train', from_file=True, save=False, 
+             file='./jaad_train_I15_O15_S15_shuffled.csv',
+             save_path='',
+             input=15, output=15, stride=15, skip=1, task='bounding_box-intention-body_pos', use_scenes=False):
+
+       self.batch_size = batch_size
+       self.loader_shuffle = loader_shuffle
+       self.pin_memory = pin_memory
+       self.loader_workers = loader_workers
+       self.load_checkpoint = load_checkpoint
+       self.jaad_dataset = jaad_dataset
+       self.dtype = dtype
+       self.from_file = from_file
+       self.save = save
+       self.file = file
+       self.save_path = save_path
+       self.input = input
+       self.output = output
+       self.stride = stride
+       self.skip = skip
+       self.task = task
+       self.use_scenes = use_scenes
+
+args = Args()
+
+#defining the model
+model = LSTM(pose_dim=34, h_dim=64, num_layers=1, dropout=0.).to(device).double()
+loss_fn = nn.MSELoss() 
+optimizer = optim.Adam(model.parameters(), lr=0.1)
+#optimizer = optim.SGD(model.parameters(), lr=0.1)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=15, min_lr=1e-5, verbose=True)
+
+if(args.load_checkpoint):
+    model.load_state_dict(torch.load(filename_save))
+
+#loading the data
+args.dtype = "train"
+train_loader = DataLoader.data_loader(args)
+args.dtype = 'val'
+args.save_path = args.save_path.replace('train', 'val')
+args.file = args.file.replace('train', 'val')
+val_loader = DataLoader.data_loader(args)
+
+
+print('Training ...')
+training_loop(n_epochs=10000,
+           optimizer=optimizer, 
+           scheduler=scheduler,
+           model=model,
+           loss_fn=loss_fn,
+           train_loader=train_loader,
+           val_loader=val_loader)
+
